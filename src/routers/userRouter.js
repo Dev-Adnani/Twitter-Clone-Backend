5a3985736564ca5efeca605df1293045a84fe5a8
Dev-Adnani <devadnani26@gmail.com>
2023-03-06 15:54:24 +0530

    Following... Done

--- src/routers/userRouter.js
+++ src/routers/userRouter.js
@@ -110,6 +110,35 @@ userRouter.post("/users/me/avatar" ,auth ,imgUpload.single("avatar"), async (req
 
 //---------------POST ENDS-----------------//
 
+//---------------PUT-----------------//
+
+
+userRouter.put("/users/:id/follow" , auth ,async (req, res) => {
+  
+  if(req.user.id != req.params.id)
+  {
+    try {
+      const user = await User.findById(req.params.id); 
+      if(!user.followers.includes(req.user.id))
+      {
+        await user.updateOne({$push:{followers:req.user.id}});
+        await req.user.updateOne({$push:{followings:req.params.id}});
+        res.status(200).json("User Has Been Followed");
+      }
+      else
+      {
+        res.status(200).json("Your Are Already Following The User");
+      }
+    } catch (error) {
+      res.status(500).json(error)
+    }
+  }
+  else
+  {
+    res.status(403).json("You Cannot Follow Yourself ")
+  }
+});
+//---------------PUT ENDS-----------------//
 
 //delete
 userRouter.delete("/users/del/:id", async (req, res) => {
